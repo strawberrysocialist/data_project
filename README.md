5784c8eb7f46f1d1fcb002be992ac569aafafcaf
strawberrysocialist <strawberrysocialist@hotmail.com>
2014-08-24 13:03:52 -0700

    Final version; confirmed working on shinyapps.io

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 data_project
 ============
+
+## Effects of Costs on Returns
+
+With everyone responsible for funding theri own retirement, it is important to make sound financial decisions. Many retail investors do not fully understand the long term effects of even small differences in costs on returns between different assets.  
+
+YOu can explore this relationship using this tool. Just enter the following figures:
+1. an initial investment amount; 
+2. the estimated long run average rate of return for the investment; and 
+3. the year you intend to recoup your returns.  
+
+The average costs for ETFs and mutual funds were taken from the [Wall Street Journal article](http://guides.wsj.com/personal-finance/investing/how-to-choose-an-exchange-traded-fund-etf/).
--- server.R
+++ server.R
@@ -1,10 +1,96 @@
-df=NULL
+get.returns.table <- function(investment, avg.return, sell.year, pretty.print=FALSE) {
+  value <- function(period) {
+    current.value = investment * ((1 + (avg.return - annual.expense))) ^ period
+    if (pretty.print) {
+      current.value = format(current.value, nsmall=2, 
+                             decimal.mark=".", big.mark=",")
+    }
+    current.value
+  }
+  
+  period = 1
+  sell.year = input$sell.year
+  #buy.year = strptime(date(), "%a %b %d %H:%M:%S %Y")
+  #strptime fails on shinyapps.io server
+  #parsing the string works
+  buy.year <- date()
+  buy.year = as.numeric(substr(buy.year, 
+                               nchar(buy.year)-4+1, 
+                               nchar(buy.year)))
+  #final.period = sell.year - 1900 - buy.year$year
+  #final.period = sell.year #TESTING-WORKS
+  #final.period = sell.year - 1900 #TESTING-WORKS
+  #final.period = buy.year$year #TESTING-FAILS
+  #final.period = buy.year #TESTING-FAILS
+  final.period = sell.year - buy.year
+  #sell.year = buy.year #UNNECESSARY
+  #sell.year$year = buy.year$year + final.period #UNNECESSARY
+  initial.expense = 10
+  final.expense = 10
+  periods = period:final.period
+  
+  df = as.data.frame(periods)
+  colnames(df) = c("Periods")
+  df.expenses = as.data.frame(c("Zero Expenses",
+                                "Small US ETF","Large US ETF",
+                                "Passive MF","Passive Taxable MF",
+                                "Active MF","Active Taxable MF"))
+  colnames(df.expenses) = c("Fund Type")
+  df.expenses$Expense = 0
+  
+  #Zero Expenses
+  annual.expense = 0
+  df.expenses[1,2] = paste0(annual.expense * 100, ".00%")
+  df$Zero_Expenses = value(periods)
+  #ETF Small US
+  annual.expense = 0.0044 + 0.0191
+  df.expenses[2,2] = paste0(annual.expense * 100, "%")
+  df$Small_US_ETF = value(periods)
+  #ETF Large US
+  annual.expense = 0.0044 + 0.0021
+  df.expenses[3,2] = paste0(annual.expense * 100, "%")
+  df$Large_US_ETF = value(periods)
+  #MF Passive
+  annual.expense = 0.0074 + 0.0144 + 0.0083
+  df.expenses[4,2] = paste0(annual.expense * 100, "%")
+  df$Passive_MF = value(periods)
+  #MF Passive Taxable
+  annual.expense = 0.0074 + 0.0144 + 0.0083 + 0.01
+  df.expenses[5,2] = paste0(annual.expense * 100, "%")
+  df$Passive_MF_Tax = value(periods)
+  #MF Active
+  annual.expense = 0.0090 + 0.0144 + 0.0083
+  df.expenses[6,2] = paste0(annual.expense * 100, "%")
+  df$Active_MF = value(periods)
+  #MF Active Taxable
+  annual.expense = 0.0090 + 0.0144 + 0.0083 + 0.01
+  df.expenses[7,2] = paste0(annual.expense * 100, "%")
+  df$Active_MF_Tax = value(periods)
+  df.expenses
+  
+  colnames(df) = c("Periods", "Zero Expenses",
+                   "Small US ETF","Large US ETF",
+                   "Passive MF","Passive Taxable MF",
+                   "Active MF","Active Taxable MF")
+  df
+}
 
 shinyServer(function(input, output) {
   output$intro <- renderText({
-    buy.year = strptime(date(), "%a %b %d %H:%M:%S %Y")
     sell.year = input$sell.year
-    final.period = sell.year - 1900 - buy.year$year
+    #buy.year = strptime(date(), "%a %b %d %H:%M:%S %Y")
+    #strptime fails on shinyapps.io server
+    #parsing the string works
+    buy.year <- date()
+    buy.year = as.numeric(substr(buy.year, 
+                                 nchar(buy.year)-4+1, 
+                                 nchar(buy.year)))
+    #final.period = sell.year - 1900 - buy.year$year
+    #final.period = sell.year #TESTING-WORKS
+    #final.period = sell.year - 1900 #TESTING-WORKS
+    #final.period = buy.year$year #TESTING-FAILS
+    #final.period = buy.year #TESTING-FAILS
+    final.period = sell.year - buy.year
     
     paste0("Investing $", 
            format(input$investment, nsmall=2, decimal.mark=".", big.mark=","),
@@ -12,75 +98,18 @@ shinyServer(function(input, output) {
            format(input$avg.return * 100, nsmall=2, decimal.mark="."),
            "% for ",
            final.period,
-           " years would grow to the following amounts for the different asset types."
+           " years would grow to the following amounts for the different fund types."
     )
   })
   output$returnsTable <- renderTable({
-      value <- function(period) {
-        current.value = investment * ((1 + (avg.return - annual.expense))) ^ period
-        current.value = format(current.value, nsmall=2, decimal.mark=".", big.mark=",")
-        current.value
-      }
-      
-      investment = input$investment
-      avg.return = input$avg.return
-      period = 1
-      buy.year = strptime(date(), "%a %b %d %H:%M:%S %Y")
-      sell.year = input$sell.year
-      final.period = sell.year - 1900 - buy.year$year
-      sell.year = buy.year
-      sell.year$year = buy.year$year + final.period
-      initial.expense = 10
-      final.expense = 10
-      periods = period:final.period
-      
-      df = as.data.frame(periods)
-      colnames(df) = c("Periods")
-      df.expenses = as.data.frame(c("Zero Expenses",
-                                    "Small US ETF","Large US ETF",
-                                    "Passive MF","Passive Taxable MF",
-                                    "Active MF","Active Taxable MF"))
-      colnames(df.expenses) = c("Asset Type")
-      df.expenses$Expense = 0
-      
-      #Zero Expenses
-      annual.expense = 0
-      df.expenses[1,2] = paste0(annual.expense * 100, ".00%")
-      df$Zero_Expenses = value(periods)
-      #ETF Small US
-      annual.expense = 0.0044 + 0.0191
-      df.expenses[2,2] = paste0(annual.expense * 100, "%")
-      df$Small_US_ETF = value(periods)
-      #ETF Large US
-      annual.expense = 0.0044 + 0.0021
-      df.expenses[3,2] = paste0(annual.expense * 100, "%")
-      df$Large_US_ETF = value(periods)
-      #MF Passive
-      annual.expense = 0.0074 + 0.0144 + 0.0083
-      df.expenses[4,2] = paste0(annual.expense * 100, "%")
-      df$Passive_MF = value(periods)
-      #MF Passive Taxable
-      annual.expense = 0.0074 + 0.0144 + 0.0083 + 0.01
-      df.expenses[5,2] = paste0(annual.expense * 100, "%")
-      df$Passive_MF_Tax = value(periods)
-      #MF Active
-      annual.expense = 0.0090 + 0.0144 + 0.0083
-      df.expenses[6,2] = paste0(annual.expense * 100, "%")
-      df$Active_MF = value(periods)
-      #MF Active Taxable
-      annual.expense = 0.0090 + 0.0144 + 0.0083 + 0.01
-      df.expenses[7,2] = paste0(annual.expense * 100, "%")
-      df$Active_MF_Tax = value(periods)
-      df.expenses
-      
-      colnames(df) = c("Periods", "Zero Expenses",
-                       "Small US ETF","Large US ETF",
-                       "Passive MF","Passive Taxable MF",
-                       "Active MF","Active Taxable MF")
-      df[nrow(df),2:ncol(df)]
+    df <- get.returns.table(input$investment, 
+                            input$avg.return, 
+                            input$sell.year, 
+                            TRUE)
+    df[nrow(df),2:ncol(df)]
   })
   output$costs <- renderText({
-    "The average cumulative annual costs for each asset type are as follows:"
+    "The average cumulative annual costs for each fund type are as follows:"
   })
   output$costsTable <- renderTable({
     df.expenses = as.data.frame("Total Expenses (%)")
@@ -119,64 +148,20 @@ shinyServer(function(input, output) {
   output$returnsChart <- renderPlot({
     library(ggplot2)
     library(reshape2)
-    value <- function(period) {
-      current.value = investment * ((1 + (avg.return - annual.expense))) ^ period
-      #current.value = format(current.value, nsmall=2, decimal.mark=".", big.mark=",")
-      current.value
-    }
-    
-    investment = input$investment
-    avg.return = input$avg.return
-    period = 1
-    buy.year = strptime(date(), "%a %b %d %H:%M:%S %Y")
-    sell.year = input$sell.year
-    final.period = sell.year - 1900 - buy.year$year
-    sell.year = buy.year
-    sell.year$year = buy.year$year + final.period
-    initial.expense = 10
-    final.expense = 10
-    periods = period:final.period
-    
-    df = as.data.frame(periods)
-    colnames(df) = c("Periods")
-    
-    #Zero Expenses
-    annual.expense = 0
-    df$Zero_Expenses = value(periods)
-    #ETF Small US
-    annual.expense = 0.0044 + 0.0191
-    df$Small_US_ETF = value(periods)
-    #ETF Large US
-    annual.expense = 0.0044 + 0.0021
-    df$Large_US_ETF = value(periods)
-    #MF Passive
-    annual.expense = 0.0074 + 0.0144 + 0.0074 + 0.0083
-    df$Passive_MF = value(periods)
-    #MF Passive Taxable
-    annual.expense = 0.0074 + 0.0144 + 0.0074 + 0.0083 + 0.01
-    df$Passive_MF_Tax = value(periods)
-    #MF Active
-    annual.expense = 0.0090 + 0.0144 + 0.0074 + 0.0083
-    df$Active_MF = value(periods)
-    #MF Active Taxable
-    annual.expense = 0.0090 + 0.0144 + 0.0074 + 0.0083 + 0.01
-    df$Active_MF_Tax = value(periods)
-    colnames(df) = c("Periods", "Zero Expenses",
-                     "Small US ETF","Large US ETF",
-                     "Passive MF","Passive Taxable MF",
-                     "Active MF","Active Taxable MF")
-    
+    df <- get.returns.table(input$investment, 
+                            input$avg.return, 
+                            input$sell.year)
     df <- melt(df,id=c("Periods"))
-    colnames(df) = c("Year", "Asset_Type",
+    colnames(df) = c("Year", "Fund_Type",
                           "Value")
-    qplot(Year,Value,data=df,colour=Asset_Type,
-          geom="line",main="Mutual Fund/ETF Perfromance")
+    qplot(Year,Value,data=df,colour=Fund_Type,
+          geom="line",main="ETF/Mutual Fund Perfromance")
   })
   output$outro <- renderText({
     paste("Hence, when you make investment decisions pay careful",
-      " attention to all fees associated with the type of asset.",
+      " attention to all fees associated with the type of fund.",
       " In general ETFs are a much cheaper way of diversifying.",
-      " However, if you but buy and sell often the transaction",
+      " However, if you buy and sell often the transaction",
       " costs will eat into your returns.")
   })
 })
